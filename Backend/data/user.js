463e4124d3a235e3deb3e0b1f5602584de39f0a0
Rayers <ranjitkarrayers@gmail.com>
2026-07-20 11:47:11 +0545

    creating model, controller for the user

--- Backend/data/user.js
+++ Backend/data/user.js
@@ -1,10 +1,22 @@
 import mongoose from "mongoose";
+import bycrypt from 'bcrypt';
 
 const userSchema = mongoose.Schema({
     name: { type: String, required: true, trim:true }, // User name, not email address!
     email: { type: String, required: true, trim:true, unique: true },
-    password: { tupe: String, minimum: 6, reqiured: true, trim: true},
+    password: { tupe: String, minimum: 6, reqiured: true, trim: true}, //we need to hash the password using bycrypt (1st : npm install bycrypt)
     isAdmin: {type: Boolean, default: false},
 },
-{timestamps: { createdAt: "created_at", updatedAt: "updated_at" }}
+{timestamps: true }
 );
+
+const user = moongose.model('UserModel',userSchema,'userTable'); // mongoose.model(modelName, ourSchema, collectionAkaTableName')
+
+//User create hunu bhanda aagadi nai hash hunu paryo so we use 'Pre' function given by moongoose
+user.pre('save', ()=>{
+    if(this.modified('password') ){
+        this.password = bcrypt.hashSync(this.password, 10); //this is 10 times hashing
+    }
+})
+
+export default user;
